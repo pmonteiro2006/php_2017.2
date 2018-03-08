faecdf407285f54e52ccc73014791e36e4825159
51115152017.2 <51115152017.2@51DE194272.senacrjedu.local>
2018-03-08 18:17:13 -0300

    Conteúdo da aula 02

---
+++ tipo_dados.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * Autor: Paulo Monteiro
+ * Data: 06-03-2018
+ * Finalidade: Explanação sobre os tipos de dados em PHP
+ */
+
+    # Variável armazenando um valor texto
+    $texto = "Segunda aula de PHP";
+    # Variável armazenando um valor inteiro
+    $inteiro = 02;
+    # Variável armazenando um valor real
+    $real = 45.15;
+    # Variável armazenando um valor caractere
+    $letra = 'L';
+    # Variável armazenando um valor boleano
+    $boleano = TRUE;
+    
+    var_dump($texto);
+    echo "<br>";
+    var_dump($inteiro);
+    echo "<br>";
+    var_dump($real);
+    echo "<br>";
+    var_dump($letra);
+    echo "<br>";
+    var_dump($boleano);
+    
